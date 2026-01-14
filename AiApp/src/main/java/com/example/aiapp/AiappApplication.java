9a0183989159b34d9f48f872b500dc608c2bd803
RanjeetPandhare24 <154042072+RanjeetPandhare24@users.noreply.github.com>
2026-01-14 12:01:27 +0530

    Updated AiappApplication.java

--- AiApp/src/main/java/com/example/aiapp/AiappApplication.java
+++ AiApp/src/main/java/com/example/aiapp/AiappApplication.java
@@ -1,13 +1,12 @@
 package com.example.aiapp;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AiappApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(AiappApplication.class, args);
-	}
-
+public class AiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AiApplication.class, args);
+    }
 }
